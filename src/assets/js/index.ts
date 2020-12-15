e04dfa3ca398e1e50697ec8c86947f2c779caf0a
Tomek <szafran1308@gmail.com>
2020-12-15 22:10:47 +0100

    Fixed server ip

--- src/assets/js/index.ts
+++ src/assets/js/index.ts
@@ -35,4 +35,4 @@ document.addEventListener('keydown', (event) => {
 
 export let game: Game;
 export let CTX: any;
-export const serverIp = '159.65.115.115:80/server'
+export const serverIp = '159.65.115.115/server'
