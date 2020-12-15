c004759230541e6166305eb3e57f2697a4f3a20b
Tomek <szafran1308@gmail.com>
2020-12-15 22:16:08 +0100

    Fixed server ip

--- src/views/GameComponent.vue
+++ src/views/GameComponent.vue
@@ -52,7 +52,7 @@
     import { serverIp } from "@/assets/js";
 
     // @ts-ignore
-    export const playerSocket: SocketIO.Socket = io(`http://159.65.115.115/DSADSASADASAS`)
+    export const playerSocket: SocketIO.Socket = io(`http://159.65.115.115//DSADSASADASAS/`)
     //export let game: any
 
     @Component({
